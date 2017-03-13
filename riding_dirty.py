e36b8c263442c45f35329ecca9ac940bb4119955
David Turner <david.w.turner@cl.cam.ac.uk>
2017-03-13 17:09:27 +0000

    reformatted output

--- riding_dirty.py
+++ riding_dirty.py
@@ -14,32 +14,34 @@ def check_repo(path):
     Check if the repository has any dirty changes or commits which have not
     been pushed to any of its remotes.
     """
+    repo_name = path.split("/")[-1]
+    printv("Repo {}".format(repo_name))
     repo = Repo(path)
     if repo.bare:
         return
     if repo.is_dirty():
-        print("{} is dirty (has uncommitted changes)".format(path))
+        print("Repo {} is dirty (has uncommitted changes)".format(repo_name))
     if len(repo.untracked_files) != 0:
-        print("{} has untracked files".format(path))
+        print("Repo {} has untracked files".format(repo_name))
 
     # Foreach local branch, foreach remote:
     #  Check if the remote has a copy of this branch
     #  If so, check if its commit matches our local one.
-    for head in repo.heads:
-        printv("Checking checkout {}".format(head))
-        for remote in repo.remotes:
-            printv("  Checking remote {}".format(remote))
-            if head.name in remote.refs:
-                printv("    Matched {}".format(head.name))
-                if head.commit == remote.refs[head.name].commit:
-                    printv("    Remote {} is up to date with branch {}"
-                           "".format(remote, head))
+    for remote in repo.remotes:
+        printv("Repo {} remote {}".format(repo_name, remote))
+        for branch in repo.heads:
+            if branch.name in remote.refs:
+                printv("Repo {} remote {} branch {}".format(repo_name, remote,
+                                                            branch))
+                if branch.commit == remote.refs[branch.name].commit:
+                    printv("Repo {} remote {} branch {}: Up to date".format(
+                        repo_name, remote, branch))
                 else:
-                    print("    Remote {} branch {} doesn't match local".format(
-                        remote, head))
+                    print("Repo {} remote {} branch {}: Mismatch!".format(
+                        repo_name, remote, branch))
             else:
-                print("  Remote {} doesn't have branch {}".format(remote,
-                                                                  head))
+                print("Repo {} remote {} branch {}: Remote doesn't have branch"
+                      "".format(repo_name, remote, branch))
 
 
 if __name__ == "__main__":
